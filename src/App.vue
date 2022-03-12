14eb2741a794596de04be9c19c522e65a2cc9213
Cupkiller <970506352@qq.com>
2022-03-12 15:29:40 +0800

    登录界面完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <login></login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import login from "./pages/login";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    login,
+  },
+};
 </script>
 
 <style>
+* {
+  padding: 0px;
+  margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  border: hidden;
 }
 </style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :style="conTop">
+      <el-form label-position="left" :model="form" label-width="75px">
+        <el-form-item label="用户名">
+          <el-input placeholder="请输入用户名" v-model="form.username" prefix-icon="el-icon-user-solid"> </el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input placeholder="请输入密码" v-model="form.userpassword" show-password prefix-icon="el-icon-s-opportunity"> </el-input>
+        </el-form-item>
+        <el-button round type="primary" @click="onSubmit"> 登录 </el-button>
+      </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      form: {
+        username: "",
+        userpassword: "",
+      },
+      conTop: {
+        display: "flex",
+        position: "absolute",
+        backgroundImage: "url(" + require("./Desert.jpg") + ")",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("登录成功");
+    },
+  },
+};
+</script>
+
+<style>
+.el-form {
+  margin: auto;
+}
+</style>
